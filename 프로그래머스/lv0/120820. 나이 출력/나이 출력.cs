b941ed7e5a54e99d1d5c57c0150248493422d6ec
오승재 <74169285+wmtfwa@users.noreply.github.com>
2023-04-13 13:13:59 +0900

    [level 0] Title: 나이 출력, Time: 0.20 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int age) {
+        int year = 2022;
+        int birth = year - age + 1;
+        
+        int answer = birth;
+        return answer;
+    }
+}
