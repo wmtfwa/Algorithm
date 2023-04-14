50acb33ab1243d5c0322e1a42f32f3f85cf455a4
오승재 <74169285+wmtfwa@users.noreply.github.com>
2023-04-14 16:00:12 +0900

    [level 0] Title: 배열의 평균값, Time: 0.17 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        
+        foreach(int num in numbers)
+            answer += num;
+        
+        answer = answer / numbers.Length;
+        
+        return answer;
+    }
+}
