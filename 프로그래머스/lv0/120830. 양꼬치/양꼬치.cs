59ab58177ad5e0918ac5bc2bb446d27efcf4c42c
오승재 <74169285+wmtfwa@users.noreply.github.com>
2023-04-14 15:56:45 +0900

    [level 0] Title: 양꼬치, Time: 0.18 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        answer = 12000 * n + 2000 * (k - (n/10));
+        
+        return answer;
+    }
+}
