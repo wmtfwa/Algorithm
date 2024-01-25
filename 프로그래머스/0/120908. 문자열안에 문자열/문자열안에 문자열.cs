ef1381bfbb897a176eeadcab5e7493a70c827d67
오승재 <74169285+wmtfwa@users.noreply.github.com>
2024-01-25 14:57:20 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.22 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cs
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cs
@@ -3,12 +3,8 @@ using System;
 public class Solution {
     public int solution(string str1, string str2) {
         int answer = 0;
-        int k = 0;
         
-        if(str1.Contains(str2))
-            answer = 1;
-        else
-            answer = 2;
+        answer = str1.Contains(str2) ? 1 : 2;
         
         return answer;
     }
