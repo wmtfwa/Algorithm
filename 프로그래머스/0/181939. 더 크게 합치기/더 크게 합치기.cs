05078c3a2209b9c601e369dfe9a246dec26aaeb9
오승재 <74169285+wmtfwa@users.noreply.github.com>
2024-01-30 17:24:48 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.80 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cs
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cs
@@ -2,22 +2,10 @@ using System;
 
 public class Solution {
     public int solution(int a, int b) {
-        int answer = 0;
         
-        String c = a.ToString();
-        String d = b.ToString();
+        int num1 = Int32.Parse($"{a}{b}");
+        int num2 = Int32.Parse($"{b}{a}");
         
-        String n1 = c + d;
-        String n2 = d + c;
-        
-        int num1 = Int32.Parse(n1);
-        int num2 = Int32.Parse(n2);
-        
-        if(num1 > num2)
-            answer = num1;
-        else
-            answer = num2;
-        
-        return answer;
+        return Math.Max(num1,num2);
     }
 }
