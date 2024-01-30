5ae6b0e34f9b711df4b8c90448747adc7c3c82af
오승재 <74169285+wmtfwa@users.noreply.github.com>
2024-01-30 15:56:19 +0900

    [level 0] Title: 공배수, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int number, int n, int m) {
+        
+        if(number % n == 0 && number % m == 0)
+            return 1;
+        else
+            return 0;
+    }
+}
