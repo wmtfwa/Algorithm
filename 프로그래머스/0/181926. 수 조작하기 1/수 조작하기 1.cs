f478229e0bed2f418eefda2aca728f357ab7b185
오승재 <74169285+wmtfwa@users.noreply.github.com>
2024-01-31 11:44:39 +0900

    [level 0] Title: 수 조작하기 1, Time: 0.44 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Solution {
+    public int solution(int n, string control) {
+        
+        for(int i = 0; i < control.Length; i++)
+        {
+            switch(control[i])
+            {
+                case 'w':
+                    n += 1;
+                    break;
+                case 'a':
+                    n -= 10;
+                    break;
+                case 's':
+                    n -= 1;
+                    break;
+                case 'd':
+                    n += 10;
+                    break;
+            }
+        }
+        return n;
+    }
+}
