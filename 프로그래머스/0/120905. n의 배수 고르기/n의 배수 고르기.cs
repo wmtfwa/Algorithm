8fb6f444272cb148ad9cccee3841ec7022601b3f
오승재 <74169285+wmtfwa@users.noreply.github.com>
2024-01-31 17:52:44 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.50 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int n, int[] numlist) {
+        List<int> answer = new List<int>();
+        
+        for(int i = 0; i < numlist.Length; i++)
+        {
+            if(numlist[i] % n == 0)
+                answer.Add(numlist[i]);
+        }
+        
+        return answer.ToArray();
+    }
+}
