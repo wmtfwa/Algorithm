2c0dcab1e9574d8f27ba32b0d208ffd6eac0b6e6
오승재 <74169285+wmtfwa@users.noreply.github.com>
2024-03-08 11:39:40 +0900

    [level 0] Title: 369게임, Time: 0.18 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+    public int solution(int order) {
+        int answer = 0;
+
+        while(order > 0)
+        {
+            if((order % 10) % 3 == 0 && order % 10 != 0)
+            {
+                answer++;
+            }
+            order /= 10;
+        }
+        
+        return answer;
+    }
+}
