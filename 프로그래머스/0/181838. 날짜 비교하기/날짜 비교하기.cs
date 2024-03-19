f40acc8bfd9eafb5984f8fc938c655272c028298
오승재 <74169285+wmtfwa@users.noreply.github.com>
2024-03-19 14:26:27 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.89 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int[] date1, int[] date2) {
+        
+        int d1 = int.Parse($"{date1[0]}{date1[1]}{date1[2]}");
+        int d2 = int.Parse($"{date2[0]}{date2[1]}{date2[2]}");
+        
+        return d1 < d2 ? 1 : 0;
+    }
+}
